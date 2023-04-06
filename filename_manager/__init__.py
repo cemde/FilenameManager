15e6959c9c483a5ac17d332cb22f3e0528905c8f
cemde <c.emde@me.com>
2023-04-06 15:25:22 +0100

    removed jinja2 dependency

--- filename_manager/__init__.py
+++ filename_manager/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Cornelius Emde"""
 __email__ = 'c.emde@me.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from .filename_manager import FilenameManager
 from .parameter import Parameter
